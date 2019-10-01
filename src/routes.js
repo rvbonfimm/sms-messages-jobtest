1c090bb1096d3775c37b5ba902e301a5b2a6a0f0
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-10-01 16:23:07 -0300

    Change App project structure

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,26 @@
-const routes = require("express").Router();
+const express = require("express");
+const routes = express.Router();
 
+const SystemController = require("./app/controllers/SystemController");
 const MessageController = require("./app/controllers/MessageController");
 const UserController = require("./app/controllers/UserController");
+const guestMiddleware = require("./app/middlewares/Guest");
+const authMiddleware = require("./app/middlewares/Auth");
 
-routes.get("/app/users", UserController.findAll);
-routes.get("/app/users/:id", UserController.find);
-routes.post("/app/users", UserController.create);
+routes.get("/", guestMiddleware, SystemController.index);
+routes.get("/logout", SystemController.logout);
+routes.post("/login", SystemController.login);
 
-routes.get("/app/messages", MessageController.findAll);
-routes.get("/app/messages/:id", MessageController.find);
-routes.post("/app/messages", MessageController.create);
+routes.use("/api", authMiddleware);
 
-routes.post("/app/messages/encode", MessageController.encoder);
-routes.post("/app/messages/decode", MessageController.decoder);
+routes.get("/api/users", UserController.findAll);
+routes.post("/api/users", UserController.create);
+
+routes.get("/api", MessageController.findAll);
+routes.get("/api/messages", MessageController.findAll);
+routes.get("/api/messages/:id", MessageController.find);
+routes.post("/api/messages", MessageController.create);
+
+routes.get("*", SystemController.notFound);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-const app = require("./app");
+const app = require("./index");
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Application running at " + 3000 + " port!!!");
+});
