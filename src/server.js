9908361c9e2447ec04762367421856205ca0f430
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-10-01 19:37:33 -0300

    Improvement at code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./index");
 
 app.listen(3000, () => {
-  console.log("Application running at " + 3000 + " port!!!");
+  console.log("Application running at 3000 port!!!");
 });
