5856b29039e6d6d8b9c336af403609909898501e
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-10-01 21:47:05 -0300

    Fix variables name

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  DB_HOST: process.env.MONGODB_HOST || "localhost",
-  DB_PORT: process.env.MONGODB_PORT || 27017,
-  DB_NAME: process.env.MONGODB_NAME || "smsmessages",
-  DB_USERNAME: process.env.MONGODB_USER || "",
-  DB_PASSWD: process.env.MONGODB_PASSWD || ""
+  MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
+  MONGODB_PORT: process.env.MONGODB_PORT || 27017,
+  MONGODB_NAME: process.env.MONGODB_NAME || "smsmessages",
+  MONGODB_USER: process.env.MONGODB_USER || "",
+  MONGODB_PASSWD: process.env.MONGODB_PASSWD || ""
 };
