d467c49f27dde0c7207182196ecbe083e7ab38c5
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-10-01 22:28:41 -0300

    clear

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI || "",
   MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
   MONGODB_PORT: process.env.MONGODB_PORT || 27017,
-  MONGODB_NAME: process.env.MONGODB_NAME || "smsmessages",
   MONGODB_USER: process.env.MONGODB_USER || "",
-  MONGODB_PASSWD: process.env.MONGODB_PASSWD || ""
+  MONGODB_PASSWD: process.env.MONGODB_PASSWD || "",
+  MONGODB_NAME: process.env.MONGODB_NAME || "smsmessages"
 };
