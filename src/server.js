336edf5aa96b11d218a5f3f1b0deb653123b20f1
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-10-01 21:28:13 -0300

    Add process.env.APP_HOST global variable if needed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const app = require("./index");
-
 const port = process.env.APP_PORT || 3000;
+const ipAddr = process.env.APP_HOST || "localhost";
 
 app.listen(port, () => {
-  console.log("Application running at " + port + " port!!!");
+  console.log(
+    "Application running at IP " + ipAddr + " and port " + port + "!!!"
+  );
 });
