162e39d8af1e1947e1ec3bf27277718762696678
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-10-02 14:08:31 -0300

    Add locals.user tag

--- src/app/middlewares/Auth.js
+++ src/app/middlewares/Auth.js
@@ -1,7 +1,9 @@
 module.exports = (req, res, next) => {
   if (req.session && req.session.user) {
+    res.locals.user = req.session.user;
+
     return next();
-  } else if (req.session && !req.session.user) {
-    return res.json({ error: true, message: "User not authenticated" });
   }
+
+  return res.redirect("/");
 };
